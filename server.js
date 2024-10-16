b87e7bd831ceb756649c12b17c26f10a3b43c913
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2024-10-16 09:46:50 -0500

    changed port from 3100 to 3015

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ require("./app/routes/student.routes")(app);
 require("./app/routes/lesson.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3015;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
