5faa08360ca43fce2a1c2cf0b834218d32da1854
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2024-11-05 21:03:02 -0600

    changed port number and added admin data to users

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -18,6 +18,7 @@ exports.create = (req, res) => {
     fName: req.body.fName,
     lName: req.body.lName,
     email: req.body.email,
+    admin: 0
     // refresh_token: req.body.refresh_token,
     // expiration_date: req.body.expiration_date
   };
